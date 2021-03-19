b7514034ad8f364b858a626de21fdd357b3ba3a5
Eka Nova Pramudya <78529450+pramek008@users.noreply.github.com>
2021-03-19 22:01:31 +0700

    multi_activity

---
+++ app/src/main/java/com/example/eventbuttononclick/Activitykedua.java
@@ -0,0 +1,21 @@
+package com.example.eventbuttononclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Activitykedua extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.halaman2);
+        TextView name = findViewById(R.id.tampilNameH2);
+        TextView pass = findViewById(R.id.tampilPassH2);
+
+        Bundle bundle = getIntent().getExtras();
+        name.setText(bundle.getString("data1"));
+        pass.setText(bundle.getString("data2"));
+    }
+}
--- app/src/main/java/com/example/eventbuttononclick/MainActivity.java
+++ app/src/main/java/com/example/eventbuttononclick/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.eventbuttononclick;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,34 +34,47 @@ public class MainActivity extends AppCompatActivity
             {
                 //database:)
                 //data1
-                mail1 = "cobasatu@mail";
-                sandi = "passkita";
+                mail1 = "Eka Nova Pramudya";
+                sandi = "naxpwr";
 
                 nama = edemail.getText().toString();
                 pass = edpass.getText().toString();
 
-                if(nama.equals(mail1) && sandi.equals(pass))
+                if(nama .equals("") || pass.equals("")){
+                    Toast k = Toast.makeText(getApplicationContext(),
+                            "Mohon ISI Username dan Password",Toast.LENGTH_LONG);
+                    k.show();
+                }
+//                if(nama .equals(mail1) || sandi.equals(pass))
+//                {
+//                    if(nama .equals (mail1)) {
+//                        Toast bm = Toast.makeText(getApplicationContext(),
+//                                "Password SALAH",Toast.LENGTH_SHORT);
+//                        bm.show();
+//                    }
+//                    else {
+//                        Toast bp = Toast.makeText(getApplicationContext(),
+//                                "Email SALAH",Toast.LENGTH_LONG);
+//                        bp.show();
+//                    }
+//                }
+                else if(nama.equals(mail1) && sandi.equals(pass))
                 {
                     Toast b = Toast.makeText(getApplicationContext(),
                             "Login BERHASIL",Toast.LENGTH_LONG);
                     b.show();
-                }
-                else if(nama .equals(mail1) || sandi.equals(pass))
-                {
-                    if(nama .equals (mail1)) {
-                        Toast bm = Toast.makeText(getApplicationContext(),
-                                "Password SALAH",Toast.LENGTH_SHORT);
-                        bm.show();
-                    }
-                    else {
-                        Toast bp = Toast.makeText(getApplicationContext(),
-                                "Email SALAH",Toast.LENGTH_LONG);
-                        bp.show();
-                    }
+
+                    Bundle bundle = new Bundle();
+                    bundle.putString("data1", nama.trim());
+                    bundle.putString("data2", pass.trim());
+
+                    Intent intent = new Intent(getApplicationContext(), Activitykedua.class);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
                 }
                 else {
                     Toast s2 = Toast.makeText(getApplicationContext(),
-                            "E-Mail dan Password SALAH",Toast.LENGTH_LONG);
+                            "Login GAGAL",Toast.LENGTH_LONG);
                     s2.show();
                 }
             }
